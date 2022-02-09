bd9f318aef5e8469adcab897d395bfcb1164175b
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-09 16:48:30 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import "./styles.css";
-import "./index.ts";
+import * as React from "react";
+import { render } from "react-dom";
 
-export default function App() {
+import "./styles.css";
+import { HashRouter, Route, Link, Switch } from "react-router-dom";
+
+import { Steps } from "./Steps";
+
+function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <HashRouter>
+        {/* main route component */}
+        <Switch>
+          {/* switch displays only one component that matches */}
+          <Route path="/steps/:stepId" component={Steps} />
+          {/* the div below shows when there is no match */}
+          <div>
+            <h1>Welcome, User</h1>
+            <Link to="/steps/1">Go To Steps Page</Link>
+          </div>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
